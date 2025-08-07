7d32067d593a9e96a210ab5534e4733e266271f7
aliiyuu <yu.alicia@gmail.com>
2025-08-07 11:52:50 -0400

    fix mobile layout

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: '/aliiyuu.github.io',
-  assetPrefix: '/aliiyuu.github.io/',
   images: {
     unoptimized: true,
   },
